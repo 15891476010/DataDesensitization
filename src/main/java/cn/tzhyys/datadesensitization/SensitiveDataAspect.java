a87a819c44bdca29ed1d98c34b1a2010ff27fbb9
MrZhang <1873963045@qq.com>
2024-09-28 00:10:00 +0800

    数据脱敏功能的添加和更新
    - 新增`DataDesensitizer`类，提供静态方法对对象进行脱敏处理。
    - 实现`SensitiveDataAspect`切面，处理服务层返回结果的脱敏。
    - 更新pom文件，配置新版本号和相关依赖。

---
+++ src/main/java/cn/tzhyys/datadesensitization/SensitiveDataAspect.java
@@ -0,0 +1,48 @@
+package cn.tzhyys.datadesensitization;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+
+@Aspect
+@Component
+public class SensitiveDataAspect {
+
+    @AfterReturning(pointcut = "execution(* *..service..*(..))", returning = "result")
+    public void afterReturning(Object result) throws IllegalAccessException {
+        if (result != null) {
+            handleSensitiveFields(result);
+        }
+    }
+
+    private void handleSensitiveFields(Object result) throws IllegalAccessException {
+        // 遍历对象中的所有字段，查找 @Sensitive 注解
+        for (Field field : result.getClass().getDeclaredFields()) {
+            Sensitive sensitive = field.getAnnotation(Sensitive.class);
+            if (sensitive != null) {
+                field.setAccessible(true);
+                Object fieldValue = field.get(result);
+                if (fieldValue instanceof String) {
+                    // 根据注解类型进行脱敏处理
+                    String maskedValue = mask((String) fieldValue, sensitive.type());
+                    field.set(result, maskedValue);
+                }
+            }
+        }
+    }
+
+    private String mask(String value, DesensitizationType type) {
+        switch (type) {
+            case PHONE:
+                return value.replaceAll("(\\d{3})(\\d{4})(\\d+)", "$1****$3");
+            case EMAIL:
+                return value.replaceAll("(?<=.{2}).(?=.*@)", "*");
+            case CARD:
+                return value.replaceAll("(?<=\\d{4})\\d(?=\\d{4})", "*");
+            default:
+                return value.replaceAll(".", "*");
+        }
+    }
+}
